a1f6be4878aabf30be48c724442fe3ba19dca33d
Luciano Torrano <lucianotorrano.dev@gmail.com>
2022-06-14 20:33:57 -0300

    database keys updated

--- src/keys.js
+++ src/keys.js
@@ -3,7 +3,7 @@ module.exports = {
     database: {
         host: 'us-cdbr-east-05.cleardb.net',
         user: 'bc06f6808783f2',
-        password:'ca32fe4a',
+        password:'ea90e8235a05cac',
         database: 'heroku_ba834b1ce978558'
     }
 };
